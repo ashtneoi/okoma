56519caa48588733339149f8f6306ca3d10a0fd6
ashtneoi <ashtneoi@gmail.com>
2018-03-19 15:35:00 -0700

    Generate 3*CV words

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 extern crate rand;
 
+use rand::thread_rng;
+use rand::Rng;
+
 fn main() {
-    hello();
-}
+    let syll_vowel = [
+        "a", "e", "i", "o", "u",
+        "ai", "aia", "aio", "aiu",
+        "ea", "eo", "ia", "io",
+        "oa", "oia", "oio",
+        "ua", "uia", "uio",
+    ];
+    let syll_cons = [
+        "b", "g", "k", "l", "m", "n", "r", "t", "y",
+    ];
 
+    let mut rng = thread_rng();
+    let n = 3;
 
-fn hello() {
-    println!("Today I have {} legs", rand::random::<u8>());
+    let mut word = "".to_string();
+    for _ in 0..n {
+        word.push_str(rng.choose(&syll_cons).unwrap());
+        word.push_str(rng.choose(&syll_vowel).unwrap());
+    }
+    println!("{}", word);
 }
