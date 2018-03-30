b4f71313c0be465acc60da6dbc0a6390f3c45220
ashtneoi <ashtneoi@gmail.com>
2018-03-29 22:55:07 -0700

    Allow and use non-uniform distribution
    That's probably not a real term

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 
+use std::cmp::PartialOrd;
 use std::env;
 use std::process::exit;
 use std::str::FromStr;
@@ -15,15 +16,36 @@ fn exit_with_usage() -> ! {
 
 
 fn main() {
-    let vowel = [
-        "a", "e", "i", "o", "u",
-        "ai", "aia", "aio", "aiu",
-        "ea", "eo", "ia", "io",
-        "oa", "oia", "oio",
-        "ua", "uia", "uio",
+    let vv: &[(&str, u64)] = &[
+        ("a", 10),
+        ("e", 5),
+        ("i", 8),
+        ("o", 4),
+        ("u", 2),
+        ("ai", 3),
+        ("aia", 1),
+        ("aio", 1),
+        ("aiu", 2),
+        ("ea", 1),
+        ("eo", 1),
+        ("ia", 6),
+        ("io", 3),
+        ("oa", 6),
+        ("oia", 1),
+        ("oio", 2),
+        ("ua", 2),
+        ("uia", 1),
+        ("uio", 1),
     ];
-    let cons = [
-        "b", "g", "k", "l", "m", "n", "r", "t", "y",
+    let cc: &[(&str, u64)] = &[
+        ("b", 2),
+        ("g", 1),
+        ("k", 8),
+        ("l", 7),
+        ("m", 8),
+        ("n", 7),
+        ("r", 4),
+        ("t", 4),
     ];
 
     let mut rng = thread_rng();
@@ -37,23 +59,77 @@ fn main() {
     let count = u64::from_str(&count_str).unwrap_or_else(
         |_| exit_with_usage()
     );
+    let wg = WordGen::new(vv, cc);
     for _ in 0..count {
-        println!("{}", gen_word(&vowel, &cons, &mut rng));
+        println!("{}", wg.gen(&mut rng));
     }
 }
 
 
-fn gen_word<R>(vowel: &[&str], cons: &[&str], rng: &mut R) -> String
-where R: Rng {
-    let mut word = "".to_string();
-    let mut low = 2;
-    if rng.gen() {
-        word.push_str(rng.choose(&vowel).unwrap());
-        low -= 1;
+struct PmfDist {
+    d: Vec<f64>,
+}
+
+impl PmfDist {
+    fn new(p: &[u64]) -> PmfDist {
+        let mut d: Vec<f64> = Vec::new();
+        let sum: f64 = p.iter().sum::<u64>() as f64;
+        let mut cumul_sum: u64 = 0;
+        for x in p {
+            cumul_sum += *x;
+            d.push((cumul_sum as f64) / sum);
+        }
+        assert!(p.len() == d.len());
+        PmfDist { d }
     }
-    for _ in 0..(rng.gen_range(low, low + 2)) {
-        word.push_str(rng.choose(&cons).unwrap());
-        word.push_str(rng.choose(&vowel).unwrap());
+
+    fn gen<R: Rng>(&self, rng: &mut R) -> usize {
+        let n: f64 = rng.gen_range(0.0, 1.0);
+        assert!(!n.is_nan() && !n.is_infinite()); // ?
+        let ii = self.d.binary_search_by(|x| {
+            assert!(!x.is_nan() && !x.is_infinite());
+            PartialOrd::partial_cmp(x, &n).unwrap_or_else(
+                || panic!("what?")
+            )
+        });
+        match ii {
+            Ok(i) => i,
+            Err(i) => i,
+        }
     }
-    word
+}
+
+
+struct WordGen<'a, 'b> {
+    vd: PmfDist,
+    v: Vec<&'a str>,
+    cd: PmfDist,
+    c: Vec<&'b str>,
+}
+
+
+impl<'a, 'b> WordGen<'a, 'b> {
+    fn new(vv: &[(&'a str, u64)], cc: &[(&'b str, u64)]) -> WordGen<'a, 'b> {
+        let v: (Vec<_>, Vec<_>) = vv.iter().cloned().unzip();
+        let c: (Vec<_>, Vec<_>) = cc.iter().cloned().unzip();
+        WordGen {
+            vd: PmfDist::new(&v.1),
+            v: v.0,
+            cd: PmfDist::new(&c.1),
+            c: c.0,
+        }
+    }
+
+    fn gen<R: Rng>(&self, rng: &mut R) -> String {
+        let mut word = "".to_string();
+        let mut low = 2;
+        if rng.gen() {
+            low -= 1;
+        }
+        for _ in 0..(rng.gen_range(low, low + 2)) {
+            word.push_str(self.c[self.cd.gen(rng)]);
+            word.push_str(self.v[self.vd.gen(rng)]);
+        }
+        word
+    }
 }
